c0e9896d35c0ceddbf61c3413a74ffe2d0c4aeae
chaejoolim <cjlim531@gmail.com>
2023-02-05 23:03:27 +0900

    #6-1 AppBar

--- flutter/comodoro/lib/main.dart
+++ flutter/comodoro/lib/main.dart
@@ -2,10 +2,12 @@ import 'package:comodoro/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
